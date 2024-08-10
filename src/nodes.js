7429b212487262d2ab1743c56cf90469ea88e0fc
Alirio Isea <isealirio@gmail.com>
2024-08-09 23:11:37 -0400

    add: URLs endpoints

--- src/nodes.js
+++ src/nodes.js
@@ -1,5 +1,16 @@
 const $ = (id) => document.querySelector(id);
 
+//* URLs
+const URL_BASE = 'https://api.themoviedb.org/3/';
+const URL_TRENDING = '/trending/movie/day';
+const URL_CATEGORIES_PREVIEW = '/genre/movie/list';
+const URL_CATEGORY = (id) => `/discover/movie?with_genres=${id}`;
+const URL_SEARCH = (query) => `/search/movie?query=${query}`;
+const URL_MOVIE = (id) => `/movie/${id}`;
+const URL_RELATED_MOVIES = (id) => `/movie/${id}/similar`;
+const URL_IMG = 'https://image.tmdb.org/t/p/w300';
+const URL_IMG_BACKGROUND = 'https://image.tmdb.org/t/p/w1280/';
+
 //* Sections
 const headerSection = $('#header');
 const trendingPreviewSection = $('#trendingPreview');
